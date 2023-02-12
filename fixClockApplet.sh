be0625ec4c6cd35ed7542de1e91a3fca3c6b426c
Gek Siong Low <geksiong@yahoo.com>
2023-02-12 15:08:26 +0800

    fix: resize Wav35Skin Digital clock background

--- fixClockApplet.sh
+++ fixClockApplet.sh
@@ -10,3 +10,8 @@ sudo ln -s /home/tc/ClockApplet.lua ClockApplet.lua
 cd /opt/jivelite/share/jive/applets/Wav35Skin/images/Clocks/Analog
 sudo rm wallpaper_clock_analog.png
 sudo ln -s /mnt/mmcblk0p2/tce/jive-mods/wallpaper_clock_analog.png wallpaper_clock_analog.png
+
+# Resized Digital clock background
+cd /opt/jivelite/share/jive/applets/Wav35Skin/images/Clocks/Digital
+sudo rm wallpaper_clock_digital.png
+sudo ln -s /mnt/mmcblk0p2/tce/jive-mods/wallpaper_clock_digital.png wallpaper_clock_digital.png
